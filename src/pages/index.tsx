c9be96e251dd340d7d91c8556280d9d090adc81f
mmarifat <mma.rifat66@gmail.com>
2024-01-26 13:10:55 +0000

    chore: get server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import NorthamtonComponent from "@/components/northampton";
 import CarComponent from "@/components/car-shelf";
 
-export default function Home() {
-  return (
-    <CarComponent>
-      <NorthamtonComponent />
-    </CarComponent>
-  );
+export default function Home({ data }: { data: any }) {
+    console.log(data, 'here is your data');
+    return (
+        <CarComponent>
+            <NorthamtonComponent/>
+        </CarComponent>
+    );
+}
+
+export const getServerSideProps = async (ctx) => {
+    let data = []
+    const queries = Object.keys(ctx?.query).map(k => `${k}=${ctx.query[k]}`).join('&')
+    const response = await fetch(`https://autotrader.co.uk/json/dealers/stock?advanced=true&advertising-location=at_cars&advertising-location=at_profile_cars&dealer=10038946&${queries}`);
+    if (response.ok) {
+        data = await response.json()
+    }
+    return {
+        props: {
+            data
+        }
+    }
 }

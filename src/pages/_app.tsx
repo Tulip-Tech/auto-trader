b0b0d078308471c8c5b8c666be12a35d69e3ed33
mmarifat <mma.rifat66@gmail.com>
2024-02-22 16:06:02 +0000

    chore: b&f to b&f cars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <AppLayout className={inter.className}>
             <Head>
-                <title>{pageProps?.title ? `B&F - ${capitalizeFirstLetter(pageProps.title)}` : 'B&F'}</title>
+                <title>{pageProps?.title ? `B&F Cars - ${capitalizeFirstLetter(pageProps.title)}` : 'B&F Cars'}</title>
             </Head>
             <NextNProgress color="#EB6B2A" showOnShallow={true}/>
             <Component {...pageProps} />
